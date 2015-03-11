4b1ad6a037102409d2ffb3ffec037d2ce8e6ae2e
seema84 <seemamail@web.de>
2015-03-11 21:15:19 +0100

    1.8.3 Update gson
    1.8.3 Update gson

--- org/mcteam/ancientgates/gson/typeadapters/LocationTypeAdapter.java
+++ org/mcteam/ancientgates/gson/typeadapters/LocationTypeAdapter.java
@@ -4,13 +4,13 @@ import java.lang.reflect.Type;
 
 import org.bukkit.Location;
 import org.bukkit.World;
-import org.bukkit.craftbukkit.libs.com.google.gson.JsonDeserializationContext;
-import org.bukkit.craftbukkit.libs.com.google.gson.JsonDeserializer;
-import org.bukkit.craftbukkit.libs.com.google.gson.JsonElement;
-import org.bukkit.craftbukkit.libs.com.google.gson.JsonObject;
-import org.bukkit.craftbukkit.libs.com.google.gson.JsonParseException;
-import org.bukkit.craftbukkit.libs.com.google.gson.JsonSerializationContext;
-import org.bukkit.craftbukkit.libs.com.google.gson.JsonSerializer;
+import com.google.gson.JsonDeserializationContext;
+import com.google.gson.JsonDeserializer;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParseException;
+import com.google.gson.JsonSerializationContext;
+import com.google.gson.JsonSerializer;
 import org.mcteam.ancientgates.Plugin;
 
 public class LocationTypeAdapter implements JsonDeserializer<Location>, JsonSerializer<Location> {
