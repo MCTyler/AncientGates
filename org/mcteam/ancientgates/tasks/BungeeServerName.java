c8ba8c629b5bc41b7788f54aa49d9c534836782a
seema84 <seemamail@web.de>
2015-03-12 21:10:19 +0100

    Update BungeeServerName.java

--- org/mcteam/ancientgates/tasks/BungeeServerName.java
+++ org/mcteam/ancientgates/tasks/BungeeServerName.java
@@ -4,7 +4,8 @@ import org.bukkit.scheduler.BukkitRunnable;
 import org.bukkit.entity.Player;
 import org.mcteam.ancientgates.Plugin;
 import org.mcteam.ancientgates.util.types.PluginMessage;
-import java.util.Collection;
+
+import java.util.Iterator;
 
 public class BungeeServerName extends BukkitRunnable {
  
@@ -18,13 +19,9 @@ public class BungeeServerName extends BukkitRunnable {
 	public void run() {
 		if (Plugin.bungeeServerName != null) return;
 		if (plugin.getServer().getOnlinePlayers().size() == 0) return;
-		Collection<? extends Player> players = Plugin.instance.getServer().getOnlinePlayers();
 		// Send BungeeCord "GetServer" command
 		final PluginMessage msg = new PluginMessage("GetServer");
-		for (Player player : players) {
-		player.sendPluginMessage(plugin, "BungeeCord", msg.toByteArray());
-		}
-		
+		Iterables.getFirst(Bukkit.getOnlinePlayers(), null).sendPluginMessage(plugin, "BungeeCord", msg.toByteArray());
 		// Re-schedule task to check bungeeServerName set
 		if (Plugin.bungeeServerName == null) new BungeeServerName(plugin).runTaskLater(plugin, 20L);
 	}
